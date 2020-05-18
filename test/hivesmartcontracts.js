594d9ac6909c09b49ecfd935c80b18e6b035fd86
cryptomancer <8011332+bt-cryptomancer@users.noreply.github.com>
2020-05-18 02:27:10 +0000

    fixed test case

--- test/hivesmartcontracts.js
+++ test/hivesmartcontracts.js
@@ -164,9 +164,9 @@ describe('Database', function () {
         assert.equal(genesisBlock.merkleRoot, '7048315fc8861b98fe1b2a82b86a24f80aa6e6dd225223e39771807532f5fb21');
       } else if (configFile.chainId === 'mainnet-hive'
       && CONSTANTS.UTILITY_TOKEN_SYMBOL === 'BEE') {
-        assert.equal(genesisBlock.hash, 'd6310e1f360fe0061dd4527649981f70fba6d9dc65c36fd246fc4e32d834e0ce');
-        assert.equal(genesisBlock.databaseHash, '43bdc779c64ec6d1c4a682c711371ecc4e40c7f51edb46993d83a5760edbf37f');
-        assert.equal(genesisBlock.merkleRoot, 'e8cdd4cbfe150a73146bafa6d3116b036ac23ecbaebc5939324ff504dc028063');
+        assert.equal(genesisBlock.hash, 'b4f162b6eb686e7b6943c59432a04fed2240af95ab02eccf6f2915cb43fa397c');
+        assert.equal(genesisBlock.databaseHash, '9358cdfbc5d508a188506b51b6fbcb2a1a43322bf74179665520b7dc0510f0c7');
+        assert.equal(genesisBlock.merkleRoot, '6452aef2193270163dc48cf4de6708e9dc7573f0b8467b0349b512b7b69e2914');
       }
 
       resolve();

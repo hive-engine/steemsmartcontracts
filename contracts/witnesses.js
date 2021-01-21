4d9482b3c4bfd1fe43cd8ca409f9ef076bd1a49a
Evan <eonwarped@gmail.com>
2021-01-21 14:34:55 +0000

    match inflation reward with witness contract reward

--- contracts/witnesses.js
+++ contracts/witnesses.js
@@ -8,8 +8,8 @@ const NB_WITNESSES = NB_TOP_WITNESSES + NB_BACKUP_WITNESSES;
 const NB_WITNESSES_SIGNATURES_REQUIRED = 3;
 const MAX_ROUNDS_MISSED_IN_A_ROW = 3; // after that the witness is disabled
 const MAX_ROUND_PROPOSITION_WAITING_PERIOD = 40; // 20 blocks
-const NB_TOKENS_TO_REWARD = '0.01902587';
-const NB_TOKENS_NEEDED_BEFORE_REWARDING = '0.09512935';
+const NB_TOKENS_TO_REWARD = '0.00951293'; // inflation.js tokens per block
+const NB_TOKENS_NEEDED_BEFORE_REWARDING = '0.04756465'; // 5x to reward
 // eslint-disable-next-line no-template-curly-in-string
 const UTILITY_TOKEN_SYMBOL = "'${CONSTANTS.UTILITY_TOKEN_SYMBOL}$'";
 // eslint-disable-next-line no-template-curly-in-string

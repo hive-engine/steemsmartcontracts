c63fc5045029594199a695f700a1cd1c4917f413
bennierex <bennierex@blockbrothers.io>
2022-02-24 14:49:19 +0100

    Added parameter updating Added parameter unittests

--- contracts/nftairdrops.js
+++ contracts/nftairdrops.js
@@ -1,18 +1,30 @@
 /* eslint-disable no-await-in-loop */
+/* eslint-disable max-len */
 /* global actions, api */
 
-const UTILITY_TOKEN_SYMBOL = 'BEE';
-const UTILITY_TOKEN_PRECISION = 8;
 const CONTRACT_NAME = 'nftairdrops';
 
+/* eslint-disable no-template-curly-in-string */
+// const UTILITY_TOKEN_SYMBOL = "'${CONSTANTS.UTILITY_TOKEN_SYMBOL}$'";
+// const UTILITY_TOKEN_PRECISION = "'${CONSTANTS.UTILITY_TOKEN_PRECISION}$'";
+/* eslint-enable no-template-curly-in-string */
+
+
+// BEGIN helper functions
+
+// END helper functions
+
+// BEGIN contract actions
+
 actions.createSSC = async () => {
-  const tableExists = await api.db.tableExists('pendingNftAirdrops');
+  const tableExists = await api.db.tableExists('pendingAirdrops');
   if (tableExists === false) {
-    await api.db.createTable('pendingNftAirdrops', ['nftAirdropId', 'symbol']);
+    await api.db.createTable('pendingAirdrops', ['airdropId', 'symbol']);
     await api.db.createTable('params');
 
     const params = {};
     params.feePerTransaction = '0.1';
+    params.maxTransactionsPerAirdrop = 50000;
     params.maxTransactionsPerBlock = 50;
     params.maxAirdropsPerBlock = 1;
     await api.db.insert('params', params);
@@ -23,6 +35,7 @@ actions.updateParams = async (payload) => {
   if (api.assert(api.sender === api.owner, 'not authorized')) {
     const {
       feePerTransaction,
+      maxTransactionsPerAirdrop,
       maxTransactionsPerBlock,
       maxAirdropsPerBlock,
     } = payload;
@@ -30,31 +43,28 @@ actions.updateParams = async (payload) => {
     const params = await api.db.findOne('params', {});
 
     if (feePerTransaction) {
-      if (!api.assert(typeof feePerTransaction === 'string' && !api.BigNumber(feePerTransaction).isNaN() && api.BigNumber(feePerTransaction).gte(0), 'invalid feePerTransaction')) return;
-      params.feePerTransaction = feePerTransaction;
+      if (api.assert(typeof feePerTransaction === 'string' && !api.BigNumber(feePerTransaction).isNaN() && api.BigNumber(feePerTransaction).gte(0), 'invalid feePerTransaction')) {
+        params.feePerTransaction = feePerTransaction;
+      }
+    }
+    if (maxTransactionsPerAirdrop) {
+      if (api.assert(Number.isInteger(maxTransactionsPerAirdrop) && maxTransactionsPerAirdrop > 0, 'invalid maxTransactionsPerAirdrop')) {
+        params.maxTransactionsPerAirdrop = maxTransactionsPerAirdrop;
+      }
     }
     if (maxTransactionsPerBlock) {
-      if (!api.assert(Number.isInteger(maxTransactionsPerBlock) && maxTransactionsPerBlock >= 1, 'invalid maxTransactionsPerBlock')) return;
-      params.maxTransactionsPerBlock = maxTransactionsPerBlock;
+      if (api.assert(Number.isInteger(maxTransactionsPerBlock) && maxTransactionsPerBlock > 0, 'invalid maxTransactionsPerBlock')) {
+        params.maxTransactionsPerBlock = maxTransactionsPerBlock;
+      }
     }
     if (maxAirdropsPerBlock) {
-      if (!api.assert(Number.isInteger(maxAirdropsPerBlock) && maxAirdropsPerBlock >= 1, 'invalid maxAirdropsPerBlock')) return;
-      params.maxAirdropsPerBlock = maxAirdropsPerBlock;
+      if (api.assert(Number.isInteger(maxAirdropsPerBlock) && maxAirdropsPerBlock > 0, 'invalid maxAirdropsPerBlock')) {
+        params.maxAirdropsPerBlock = maxAirdropsPerBlock;
+      }
     }
 
     await api.db.update('params', params);
   }
 };
 
-actions.newAirdrop = async (payload) => {
-  const {
-    symbol,
-    list,
-    isSignedWithActiveKey,
-  } = payload;
-
-  if (api.assert(isSignedWithActiveKey === true, 'you must use a custom_json signed with your active key')
-    && api.assert(symbol && typeof symbol === 'string'
-    && list && Array.isArray(list), 'invalid params')) {
-  }
-};
+// END contract actions
--- test/nftairdrops.js
+++ test/nftairdrops.js
@@ -16,9 +16,11 @@ const { setupContractPayload } = require('../libs/util/contractUtil');
 const { Fixture, conf } = require('../libs/util/testing/Fixture');
 const { TableAsserts } = require('../libs/util/testing/TableAsserts');
 const { assertError } = require('../libs/util/testing/Asserts');
+const { Resolver } = require('dns');
 
 const tokensContractPayload = setupContractPayload('tokens', './contracts/tokens.js');
-const contractPayload = setupContractPayload('airdrops', './contracts/nftairdrops.js');
+const nftContractPayload = setupContractPayload('nft', './contracts/nft.js');
+const contractPayload = setupContractPayload('nftairdrops', './contracts/nftairdrops.js');
 
 const fixture = new Fixture();
 const tableAsserts = new TableAsserts(fixture);
@@ -67,6 +69,94 @@ describe('NFT Airdrops Smart Contract', function () {
       resolve();
     })
       .then(() => {
+        done();
+      });
+  });
+
+  it('should update parameters', (done) => {
+    new Promise(async (resolve) => {
+
+      await fixture.setUp();
+
+      const refBlockNumber = fixture.getNextRefBlockNumber();
+      const transactions = [];
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'deploy', JSON.stringify(contractPayload)));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'nftairdrops', 'updateParams', '{"feePerTransaction": "0.3", "maxTransactionsPerAirdrop": 30001, "maxTransactionsPerBlock": 31, "maxAirdropsPerBlock": 3}'));
+
+      const block = {
+        refHiveBlockNumber: refBlockNumber,
+        refHiveBlockId: 'ABCD2',
+        prevRefHiveBlockId: 'ABCD1',
+        timestamp: '2018-06-01T00:00:00',
+        transactions,
+      };
+
+      await fixture.sendBlock(block);
+
+      // make sure there were no unexpected errors
+      await tableAsserts.assertNoErrorInLastBlock();
+
+      // parameters should have changed to the new values
+      const params = await fixture.database.findOne({
+        contract: 'nftairdrops',
+        table: 'params',
+        query: {},
+      });
+
+      assert.equal(params.feePerTransaction, '0.3');
+      assert.equal(params.maxTransactionsPerAirdrop, 30001);
+      assert.equal(params.maxTransactionsPerBlock, 31);
+      assert.equal(params.maxAirdropsPerBlock, 3);
+
+      resolve();
+    })
+      .then(() => {
+        fixture.tearDown();
+        done();
+      });
+  });
+
+  it('should not update parameters', (done) => {
+    new Promise(async (resolve) => {
+
+      await fixture.setUp();
+
+      const refBlockNumber = fixture.getNextRefBlockNumber();
+      const transactions = [];
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'deploy', JSON.stringify(contractPayload)));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'nftairdrops', 'updateParams', '{"feePerTransaction": 0.3}'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'nftairdrops', 'updateParams', '{"maxTransactionsPerAirdrop": "30001"}'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'nftairdrops', 'updateParams', '{"maxTransactionsPerBlock": null}'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'nftairdrops', 'updateParams', '{"maxAirdropsPerBlock": []}'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'nftairdrops', 'updateParams', '{"wrongKey": "whatever"}'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'bennierex', 'nftairdrops', 'updateParams', '{"feePerTransaction": "0.3", "maxTransactionsPerAirdrop": 30001, "maxTransactionsPerBlock": 31, "maxAirdropsPerBlock": 3}'));
+
+      let block = {
+        refHiveBlockNumber: refBlockNumber,
+        refHiveBlockId: 'ABCD1',
+        prevRefHiveBlockId: 'ABCD2',
+        timestamp: '2018-06-01T00:00:00',
+        transactions,
+      };
+
+      await fixture.sendBlock(block);
+
+      // check parameters remain unchanged
+      const params = await fixture.database.findOne({
+        contract: 'nftairdrops',
+        table: 'params',
+        query: {},
+      });
+
+      assert.equal(params.feePerTransaction, '0.1');
+      assert.equal(params.maxTransactionsPerAirdrop, 50000);
+      assert.equal(params.maxTransactionsPerBlock, 50);
+      assert.equal(params.maxAirdropsPerBlock, 1);
+
+      resolve();
+    })
+      .then(() => {
+        fixture.tearDown();
         done();
       });
   });
